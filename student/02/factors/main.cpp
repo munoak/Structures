8db10a937b4ed39c33fb962ad358656cf3f0d1b7
Noman Akbar <mknoak@linux-desktop14.tuni.fi>
2022-09-18 02:39:05 +0300

    Resolved error of test zero

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -5,28 +5,29 @@ using namespace std;
 
 int main()
 {
-    int number {};
+    int number {0};
     vector <int> factors {};
     cout << "Enter a positive number: ";
     cin >> number;
-    if(number <=0)
-        cout << "Only positive numbers accepted" << endl;
+    if(number <= 0)
+        std::cout << "Only positive numbers accepted" << endl;
     else
     {
-    for(int i=1; i <= number; i++)
-    {
+        for(int i=1; i <= number; i++)
+        {
         if(number%i == 0)
-        factors.push_back(i);
+            factors.push_back(i);
+            }
+        auto mid = factors.size()/2;
+        if (factors.size() % 2 == 0)
+        {
+        cout << number << " = " << factors.at(mid-1) << " * " << factors.at(mid) << endl;
+        }
+        else
+        {
+        cout << number << " = " << factors.at(mid) << " * " << factors.at(mid) << endl;
+        return 0;
+        }
     }
-    }
-auto mid = factors.size()/2;
-if (factors.size() % 2 == 0)
-{
-cout << number << " = " << factors.at(mid-1) << " * " << factors.at(mid) << endl;
-}
-else
-{
-    cout << number << " = " << factors.at(mid) << " * " << factors.at(mid) << endl;
-}
-return 0;
+
 }

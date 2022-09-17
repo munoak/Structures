3e679d3be257259b22c767d6fbc51db85edc59c2
Noman Akbar <mknoak@linux-desktop8.tuni.fi>
2022-09-17 03:18:58 +0300

    Assignment2_temp

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
+    double temp {};
+    double celsius {};
+    double frnht {};
     cout << "Enter a temperature: ";
-
-    // Write your code here
+    cin >> temp;
+    frnht = (temp * 1.8) + 32;
+    celsius = (temp -32) * 5/9;
+    cout << temp << " degrees Celsius is " << frnht << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << celsius << " degrees Celsius" << endl;
 
     return 0;
 }

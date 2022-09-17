45dc37dfdfdea27c2c184fa243dd623266abb684
Noman Akbar <mknoak@linux-desktop14.tuni.fi>
2022-09-18 01:16:15 +0300

    Printing nearest factors of a number

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+    int number {};
+    vector <int> factors {};
     cout << "Enter a positive number: ";
-
-    // Write your code here
-
-    return 0;
+    cin >> number;
+    if(number <=0)
+        cout << "Only positive numbers accepted" << endl;
+    else
+    {
+    for(int i=1; i <= number; i++)
+    {
+        if(number%i == 0)
+        factors.push_back(i);
+    }
+    }
+auto mid = factors.size()/2;
+if (factors.size() % 2 == 0)
+{
+cout << number << " = " << factors.at(mid) << " * " << factors.at(mid-1) << endl;
+}
+else
+{
+    cout << number << " = " << factors.at(mid) << " * " << factors.at(mid) << endl;
+}
+return 0;
 }

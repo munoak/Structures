1982eaca129e290cbb4d9921e6f023368310d044
Noman Akbar <mknoak@linux-desktop12.tuni.fi>
2022-09-18 18:30:32 +0300

    added full stop at the end of output

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -9,10 +9,10 @@ int main()
     cout << "Enter a number: ";
     cin >> number;
     cube = number * number * number;
-    if(cube > 0  && cube < (2e3))
-        cout << "The cube of " << number << " is " << cube << endl;
+    if(cube >= 0  && cube <= (2e10))
+        cout << "The cube of " << number << " is " << cube << "." << endl;
     else
-        cout << "Error! The cube of " << number << " is not " << cube << endl;
+        cout << "Error! The cube of " << number << " is not " << cube << "." << endl;
 
     return 0;
 }

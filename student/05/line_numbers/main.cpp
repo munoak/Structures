1158080c816a4bf41ec05f2d5e5653d2d28c7fc5
Noman Akbar <mknoak@linux-desktop14.tuni.fi>
2022-10-11 21:05:07 +0300

    adding line numbers

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<fstream>
+#include<string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+            string ch;
+            string f1;
+            cout << "Input file: ";
+            getline(cin, f1);
+            string f2 ="";
+            cout << "Output file: ";
+            getline(cin, f2);
+
+            // opening first file to read the content
+            ifstream f1_object(f1);
+            ofstream f2_object(f2);
+            if (!f1_object)
+            {
+                cout << "Error! The file " << f1 << " cannot be opened." << endl;
+                return EXIT_FAILURE;
+            }
+         else {
+                int line_num = 0;
+                while (!f1_object.eof()) {
+                    ++line_num;
+                    getline(f1_object, ch);
+                    f2_object  << line_num << " "<< ch;
+
+                }
+            }
+            // closing the files
+            f1_object.close();
+            f2_object.close();
+            return 0;
+        }

54abf8e2b1414c0fbc544ea486293f1e150a4abf
Noman Akbar <mknoak@linux-desktop10.tuni.fi>
2022-10-19 17:04:43 +0300

    palindrome recursion

---
+++ student/06/palindrome/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+bool is_palindrome(std::string str, int start, int end)
+{
+    if(start==end)
+        return true;
+    if(str[start]!=str[end])
+        return false;
+    if(start<end+1)
+    {
+        return is_palindrome(str, start+1, end-1);
+    }
+    return true;
+}
+#ifndef RECURSIVE_FUNC
+#define RECURSIVE_FUNC
+#endif
+
+bool palindrome_recursive(std::string s)
+{
+  RECURSIVE_FUNC
+  int n= 0;
+  //int f = 0;
+  n = s.size();
+     if(n!=0)
+     {
+         return is_palindrome(s, 0 , n-1);
+     }
+     return false;
+}
+
+// Do not modify rest of the code, or the automated testing won't work.
+#ifndef UNIT_TESTING
+int main()
+{
+    std::cout << "Enter a word: ";
+    std::string word;
+    std::cin >> word;
+
+    if(palindrome_recursive(word)){
+        std::cout << word << " is a palindrome" << std::endl;
+    } else {
+        std::cout << word << " is not a palindrome" << std::endl;
+    }
+}
+#endif

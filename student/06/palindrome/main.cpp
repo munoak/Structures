1482043b802cbfd2a53c9ddbd5de8692a6681b2c
Noman Akbar <mknoak@linux-desktop13.tuni.fi>
2022-10-22 04:18:05 +0300

    palindrome modified

--- student/06/palindrome/main.cpp
+++ student/06/palindrome/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
-bool is_palindrome(std::string str, int start, int end)
-{
-    if(start==end)
-        return true;
-    if(str[start]!=str[end])
-        return false;
-    if(start<end+1)
-    {
-        return is_palindrome(str, start+1, end-1);
-    }
-    return true;
-}
+//bool is_palindrome(std::string str, int start, int end)
+//{
+//    if(start==end)
+//        return true;
+//    if(str[start]!=str[end])
+//        return false;
+//    if(start<end+1)
+//    {
+//        return is_palindrome(str, start+1, end-1);
+//    }
+//    return true;
+//}
+
 #ifndef RECURSIVE_FUNC
 #define RECURSIVE_FUNC
 #endif
@@ -19,14 +20,28 @@ bool is_palindrome(std::string str, int start, int end)
 bool palindrome_recursive(std::string s)
 {
   RECURSIVE_FUNC
-  int n= 0;
-  //int f = 0;
-  n = s.size();
-     if(n!=0)
-     {
-         return is_palindrome(s, 0 , n-1);
-     }
-     return false;
+//  int n= 0;
+//  //int f = 0;
+//  n = s.size();
+//     if(n!=0)
+//     {
+//         return is_palindrome(s, 0 , n-1);
+//     }
+//     return false;
+    int x=0;
+    int n = s.size();
+    if(n==0)
+        return true;
+    if(s.at(x) != s.at(n-1))
+    {
+        return false;
+    }
+    if(s.at(x) == s.back())
+    {
+        std::string r = s.substr(x+1, n-2);
+        return palindrome_recursive(r);
+    }
+    return true;
 }
 
 // Do not modify rest of the code, or the automated testing won't work.
